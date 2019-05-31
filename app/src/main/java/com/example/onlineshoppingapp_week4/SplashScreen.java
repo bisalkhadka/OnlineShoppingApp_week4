af3d20dfc59da0bd6573c0ede3815acb60d53db5
Your Name <you@example.com>
2019-05-31 15:45:00 +0545

    this is assignment

---
+++ app/src/main/java/com/example/onlineshoppingapp_week4/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.onlineshoppingapp_week4;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.onlineshoppingapp_week4.Views.Dashboard;
+
+public class SplashScreen extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+            final Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                    finish();
+                }
+            }, 2000);
+
+    }
+}
+
+
